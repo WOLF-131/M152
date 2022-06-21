2d99c17aef615310523ef9e8b2c49640c246c074
WOLF131 <wolf131@getthewolfs.com>
2022-06-21 20:25:51 +0200

    Shaka player finish

--- src/app/livestream/livestream.component.ts
+++ src/app/livestream/livestream.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TwitchEmbed, TwitchEmbedLayout } from 'twitch-player';
 
 @Component({
   selector: 'app-livestream',
@@ -8,24 +7,7 @@ import { TwitchEmbed, TwitchEmbedLayout } from 'twitch-player';
 })
 export class LivestreamComponent implements OnInit {
   constructor() {}
-  private Twitch: any;
 
   ngOnInit(): void {
-    //   var options = {
-    //     width: 854,
-    //     height: 480,
-    //     channel: 'monstercat',
-    //     // video: "<video ID>",
-    //     // collection: "<collection ID>",
-    //   };
-    //   var player = new this.Twitch.Player('twitch-embed', options);
-    //   player.setVolume(0.5);
-
-    const embed = new TwitchEmbed('twitch-player', {
-      width: 1280,
-      height: 720,
-      channel: 'makerdeck',
-      layout: TwitchEmbedLayout.VIDEO
-    });
   }
 }
